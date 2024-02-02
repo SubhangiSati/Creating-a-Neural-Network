0aea6f525f90b0fdcd59a0044f62f325df1a78bd
Subhangi Sati <113504623+SubhangiSati@users.noreply.github.com>
2024-02-02 14:41:23 +0530

    Add files via upload

---
+++ xyz.py
@@ -0,0 +1,18 @@
+# Python program using Matplotlib
+# for forming a linear plot
+
+# importing the necessary packages and modules
+import matplotlib.pyplot as plt
+import numpy as np
+
+# Prepare the data
+x = np.linspace(0, 10, 100)
+
+# Plot the data
+plt.plot(x, x, label ='linear')
+
+# Add a legend
+plt.legend()
+
+# Show the plot
+plt.show()
